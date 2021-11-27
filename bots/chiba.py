adbb7c56a4595641295f52f299ed457bc6fa3fce
Gene Kogan <kogan.gene@gmail.com>
2021-11-27 21:42:10 +0000

    chiba bot

---
+++ bots/chiba.py
@@ -0,0 +1,24 @@
+from .channels import *
+
+chiba = {
+    'token_env': 'DISCORD_TOKEN_CHIBA',
+    'debug': False,
+    'programs': {
+        'ifttt': {
+            'key_env': 'IFTTT_KEY',
+            'actions': [
+                {'keyword': 'on', 'action': 'chibaon', 'reply': 'Chiba on!'},
+                {'keyword': 'off', 'action': 'chibaoff', 'reply': 'Chiba off!'}
+            ]
+        }
+    },    
+    'behaviors': {
+        'on_mention': {
+            'response_probability': 1.0,
+            'channels': [mc22_bots],
+            'delay': [0, 1],
+            'program': 'ifttt',
+            'reaction_probability': 0.0
+        }
+    }
+}
